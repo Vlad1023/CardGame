acc556b0c1b2bd06f1682635e9986fb8de10ca7b
volko <volkovskiy4@gmail.com>
2023-03-09 22:13:43 +0100

    add basic

--- src/main/java/com/example/cardgame/controllers/MainController.java
+++ src/main/java/com/example/cardgame/controllers/MainController.java
@@ -1,16 +1,27 @@
 package com.example.cardgame.controllers;
 
+import com.example.cardgame.models.User;
+import com.example.cardgame.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.io.Console;
 
 @Controller
 public class MainController {
-    @GetMapping(value = "/index")
-    public String index(Model model){
-        System.out.println("Hello World!");
+    @Autowired
+    UserRepository userRepository;
+    @GetMapping(value = "/")
+    public String Index(Model model){
+
+        return "index";
+    }
+    @PostMapping(value = "/addUser")
+    public String AddUser(String userName){
+        userRepository.save(new User(userName));
         return "index";
     }
 }
