85071d84331ae76eca14ac5a07620e203b691439
volko <volkovskiy4@gmail.com>
2023-03-20 12:27:21 +0100

    add web socket endpoint

--- src/main/java/com/example/cardgame/controllers/GameMenuController.java
+++ src/main/java/com/example/cardgame/controllers/GameMenuController.java
@@ -4,9 +4,13 @@ import com.example.cardgame.DTO.NewGameDTO;
 import com.example.cardgame.models.Game;
 import com.example.cardgame.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Controller
 public class GameMenuController {
@@ -18,6 +22,14 @@ public class GameMenuController {
         Game newGame = new Game(DTO.getGameName());
         newGame.AddPlayer(DTO.getUserNameHost());
         gameRepository.save(newGame);
+        addGameToList();
         return "main";
+    }
+
+    @MessageMapping("/gamesInfo")
+    @SendTo("/games/gamesList")
+    public List<Game> addGameToList(){
+        List<Game> games = (List<Game>) gameRepository.findAll();
+        return games;
     }
 }
