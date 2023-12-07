080a60eae7b41d2011ecef5965e6e41faaabf71f
volko <volkovskiy4@gmail.com>
2023-12-08 00:26:14 +0100

    Removed redundant code and reworked templates

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,16 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    main: './src/main/resources/static/game.js',
-    gameLobby: './src/main/resources/static/gameLobby.js',
     main: './src/main/resources/static/main.js',
+    gameLobby: './src/main/resources/static/gameLobby.js',
+    game: './src/main/resources/static/game.js',
+    login: './src/main/resources/static/login.js'
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'src/main/resources/static/dist'),
   },
+  optimization: {
+     minimize: false
+  }
 };
