057f8f427e9376e62e3daca7bd415db98e9d4941
volko <volkovskiy4@gmail.com>
2023-12-17 13:25:22 +0100

    Add mappings

--- src/main/java/com/example/cardgame/configuration/WebMvcConfig.java
+++ src/main/java/com/example/cardgame/configuration/WebMvcConfig.java
@@ -1,15 +1,23 @@
 package com.example.cardgame.configuration;
 
+import com.example.cardgame.DTO.GetCardDTO;
 import com.example.cardgame.DTO.GetGameDTO;
+import com.example.cardgame.DTO.GetUserDTO;
+import com.example.cardgame.models.Card;
 import com.example.cardgame.models.Game;
+import com.example.cardgame.models.User;
 import com.example.cardgame.repositories.UserRepository;
+import org.modelmapper.AbstractConverter;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
 
@@ -17,9 +25,12 @@ import java.util.Map;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     UserRepository userRepository;
+
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
+
+        // Configuration for Game to GetGameDTO mapping
         modelMapper.createTypeMap(Game.class, GetGameDTO.class)
                 .addMappings(mapper -> {
                     mapper.map(Game::getId, GetGameDTO::setGameId);
@@ -30,7 +41,31 @@ public class WebMvcConfig implements WebMvcConfigurer {
                         return players.size();
                     }).map(Game::getCurrentPlayers, GetGameDTO::setUsersCount);
                 });
+
+        // Configuration for Card to GetCardDTO mapping
+        modelMapper.createTypeMap(Card.class, GetCardDTO.class)
+                .addMappings(mapper -> {
+                    mapper.map(Card::getId, GetCardDTO::setId);
+                    mapper.map(Card::getRepresentation, GetCardDTO::setRepresentation);
+                    mapper.map(Card::getRank, GetCardDTO::setRank);
+                });
+
+        // Configuration for User to GetUserDTO mapping
+        modelMapper.createTypeMap(User.class, GetUserDTO.class)
+                .addMappings(mapper -> {
+                    mapper.map(User::getId, GetUserDTO::setUserId);
+                    mapper.map(User::getName, GetUserDTO::setName);
+                    mapper.using(getUserCardsDtoConverter()).map(User::getCurrentCards, GetUserDTO::setCurrentCards);
+                });
+
         return modelMapper;
+    }
+
+    private Converter<List<Card>, List<GetCardDTO>> getUserCardsDtoConverter() {
+        return ctx -> {
+            Type type = new TypeToken<List<GetCardDTO>>() {}.getType();
+            return modelMapper().map(ctx.getSource(), type);
+        };
     }
 
     @Override
--- src/main/java/com/example/cardgame/models/User.java
+++ src/main/java/com/example/cardgame/models/User.java
@@ -4,6 +4,10 @@ import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 @RedisHash("User")
 public class User {
     @Id
@@ -11,8 +15,15 @@ public class User {
     private String id;
     @Getter
     private String name;
+    @Getter
+    private LinkedList<Card> currentCards;
 
     public User(String name) {
         this.name = name;
+        this.currentCards = new LinkedList<>();
+    }
+
+    public void addCard(Card card) {
+        currentCards.add(card);
     }
 }
