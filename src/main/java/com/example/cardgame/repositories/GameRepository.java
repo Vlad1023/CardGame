bb151e296f4898c3b83e8a21a9c2171532950b3e
volko <volkovskiy4@gmail.com>
2024-03-12 15:50:11 +0100

    Added docker containirization and removed unused dependencies

--- src/main/java/com/example/cardgame/repositories/GameRepository.java
+++ src/main/java/com/example/cardgame/repositories/GameRepository.java
@@ -2,7 +2,6 @@ package com.example.cardgame.repositories;
 
 import com.example.cardgame.models.Game;
 import com.example.cardgame.models.User;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
